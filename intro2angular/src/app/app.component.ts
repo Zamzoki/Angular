d3e8ca17e1891b06d21451bb33985adff6c9202f
Zanoschi David <raikozvenda@yahoo.com>
2019-12-07 13:46:10 -0800

    member decorator

--- intro2angular/src/app/app.component.ts
+++ intro2angular/src/app/app.component.ts
@@ -1,28 +1,24 @@
 import { Component } from '@angular/core';
 
-function log(target, name, descriptor) {
-  console.log(target, name, descriptor);
+const log = (target, name, descriptor) => {
   // store the original function in a variable
   const original = descriptor.value;
   
   // do some manipulations with descriptor.value, which is the function that will be executed
   
-  // below, aSimpleMethod is overwritten with the method above
+  // below, aSimpleMethod is overwritten
   // descriptor.value = function() {
   //   console.log("This function was changed")
   // }
 
-  // below, I create a method in which I pass some arguments
-  descriptor.value = function(...args)
-  {
-    console.log("Arguments ", args, " were passed in this function");
+  descriptor.value = (...args: any) => {
+    // fac ca original sa apartina obiectului global, pt ca altfel ea tine de clasa AppComponent
     const result = original.apply(this, args);
-    console.log("The result of the function is ", result);
     return "Result from decorator function: " + result; 
   }
-  
-  // this call will display "Hey there!"
-  //original();
+
+  //this call displays "14"
+  console.log(original(2, 7));
   
   // return the descriptor
   return descriptor;
