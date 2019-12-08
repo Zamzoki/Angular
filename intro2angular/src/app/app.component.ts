f99450415527baec9f6cc8f727750e9418a7d8cf
Zanoschi David <raikozvenda@yahoo.com>
2019-12-08 15:31:42 -0800

    data binding
    source to target data binding
    target to source data binding

--- intro2angular/src/app/app.component.ts
+++ intro2angular/src/app/app.component.ts
@@ -8,4 +8,20 @@ import { HelloComponent } from './hello/hello.component';
 })
 export class AppComponent {
   title = 'intro2angular';
+  myVariable = Math.random().toString();
+  myDisabledValue = false;
+
+  constructor() {
+    this.myDisabledValue = false;
+
+    setInterval(() => {
+      this.myVariable = Math.random().toString()
+      this.myDisabledValue = Math.random() > 0.5
+    }, 1000);
+  }
+
+  callMyFunction() {
+    console.log("Button clicked!");
+    this.myVariable = "1";
+  }
 };
