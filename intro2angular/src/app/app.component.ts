f59fd92facc967f57c34696b30471da3883c98cf
Zanoschi David <raikozvenda@yahoo.com>
2019-12-08 14:07:54 -0800

    hello component
    created it using terminal
    edited it's html content
    used it in app component using the hello component selector

--- intro2angular/src/app/app.component.ts
+++ intro2angular/src/app/app.component.ts
@@ -1,31 +1,5 @@
 import { Component } from '@angular/core';
-
-// log as a class decorator
-const overwriteConstructorDecorator = classConstructor => {
-  return class extends classConstructor {
-    constructor(arg1: Number, arg2: Number) {
-      super(arg1, arg2);
-      let incr1 = Number (arg1) + 1;
-      let incr2 = Number (arg2) + 1;
-      console.log("Incremented in decorator: " + incr1 + ", " + incr2);
-    }
-  };
-
-  // return null; 
-}
-
-const log = classConstructor => {
-  console.log("Log something then run original constructor.");
-}
-
-@log
-class MyExampleClass {
-  constructor(arg1: Number, arg2: Number) {
-    console.log("MyExampleClass constructor call: " + arg1 + ", " + arg2);
-  };
-};
-
-const myClass = new MyExampleClass(5, 10);
+import { HelloComponent } from './hello/hello.component';
 
 @Component({
   selector: 'app-root',
@@ -34,12 +8,4 @@ const myClass = new MyExampleClass(5, 10);
 })
 export class AppComponent {
   title = 'intro2angular';
-
-  constructor() {
-    console.log("AppComponent class constructor call: ", this.aSimpleMethod(5, 2));
-  }
-
-  aSimpleMethod(a, b) {
-    return a*b;
-  }
 };
