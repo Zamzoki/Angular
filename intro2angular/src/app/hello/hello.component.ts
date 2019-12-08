3612f84bf47759aa82a2cccdc8f8f78ebfc694b0
Zanoschi David <raikozvenda@yahoo.com>
2019-12-08 14:35:19 -0800

    interpolation
    interpolating a mathematical operation
    interpolating a property value
    limits of interpolation: example of bitwise operation
    interpolating a method

--- intro2angular/src/app/hello/hello.component.ts
+++ intro2angular/src/app/hello/hello.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
+  myName = "David";
+  bitwiseOperation = 2 | 5; // 010 | 101 => 111 = 7
+  i = 0;
+  someVariable = Math.random();
+
   constructor() { }
 
+  doSomeTask() {
+    console.log(`I called this method ${this.i++} times.`);
+  }
+
   ngOnInit() {
+    // setInterval(() => {
+    //   this.someVariable = Math.random();
+    // }, 50)
   }
 
 }
