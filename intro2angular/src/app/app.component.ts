9091afdf9e75d727b8ebe29a6183471e1fa9e366
Zanoschi David <raikozvenda@yahoo.com>
2019-12-08 16:20:07 -0800

    two way data binding
    once i did it without any import, using value and input directives
    then i did it using ngModel
    example of importing a module

--- intro2angular/src/app/app.component.ts
+++ intro2angular/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HelloComponent } from './hello/hello.component';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +6,11 @@ import { HelloComponent } from './hello/hello.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'intro2angular';
-  myVariable = Math.random().toString();
-  myDisabledValue = false;
+  textVariable = "app";
+  otherTextVariable = "otherApp";
 
-  constructor() {
-    this.myDisabledValue = false;
-
-    setInterval(() => {
-      this.myVariable = Math.random().toString()
-      this.myDisabledValue = Math.random() > 0.5
-    }, 1000);
-  }
-
-  callMyFunction() {
-    console.log("Button clicked!");
-    this.myVariable = "1";
+  updateTextVariable(e) {
+    this.textVariable = e.target.value;
+    //console.log(e.target.value);
   }
 };
