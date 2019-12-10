11e7aed9d70e6f8b82dd8e06bde1674329d0afed
Zamzoki <raikozvenda@yahoo.com>
2019-12-10 12:34:05 +0200

    implemented arrow functions component

--- practiceApp/src/app/samples/arrow-functions/arrow-functions.component.ts
+++ practiceApp/src/app/samples/arrow-functions/arrow-functions.component.ts
@@ -1,16 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
+const myLogger = message => console.log(message);
+
+// working with 'this' using ES5
+function Car() {
+  var self = this;
+  this.seats = 4;
+
+  this.timeout = function() {
+    setTimeout(function() {
+      console.log(self.seats++);
+    }, 1000);
+  }
+}
+
+class CarWithArrow {
+  seats = 0;
+
+  constructor() {
+    this.seats = 6;
+  }
+
+  timeout() {
+    setTimeout(() => {
+      console.log(this.seats++);
+    }, 1000);
+  }
+}
+
 @Component({
   selector: 'app-arrow-functions',
   templateUrl: './arrow-functions.component.html',
   styleUrls: ['./arrow-functions.component.css']
 })
 export class ArrowFunctionsComponent implements OnInit {
+  
+  
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
-    
   }
 
+  ngOnInit() {
+    myLogger('Testing out arrow functions.');
+
+    var c = new Car();
+    c.timeout();
+
+    var cArrow = new CarWithArrow();
+    cArrow.timeout();
+  }
 }
