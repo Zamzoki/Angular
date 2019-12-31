621674386a6b32c1d854413492f18dcfa6474b59
Zamzoki <raikozvenda@yahoo.com>
2019-12-31 15:46:14 +0200

    implemented scopes-and-let component
    let declares variables that are limited to a scope of a block statement or expression in which the variable is used
    var keyword defines variables globally, or locally to an entire function, without regards of block scope

--- practiceApp/src/app/samples/scopes-and-let/scopes-and-let.component.ts
+++ practiceApp/src/app/samples/scopes-and-let/scopes-and-let.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+var age = 55;
+
+for (let i = 0; i < 5; i++) {
+  age += 5;
+}
+
+try {
+  // i as let is not visible here
+  // i as var would be visible
+  // console.log(i);
+} catch (e) {
+  console.log('i is out of scope due to using let!');
+}
+
 @Component({
   selector: 'app-scopes-and-let',
   templateUrl: './scopes-and-let.component.html',
@@ -10,6 +24,7 @@ export class ScopesAndLetComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
