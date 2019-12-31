a0d08e85bbe5588ca55ef5ba1fb8bc85225b7b7e
Zamzoki <raikozvenda@yahoo.com>
2019-12-31 15:51:16 +0200

    implemented template string component
    you can make at least basic operations using string concatenation using parenthesis
    operations are easier and more intuitive using template string

--- practiceApp/src/app/samples/template-strings/template-strings.component.ts
+++ practiceApp/src/app/samples/template-strings/template-strings.component.ts
@@ -10,6 +10,13 @@ export class TemplateStringsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const a = 2;
+    const b = 3;
+
+    console.log('Log a with string concatenation: ' + a);
+    console.log(`Log b with template string: ${b}`);
+    console.log('Add a and b with string concatenation: ' + (a + b));
+    console.log(`Add a and b with template string: ${a + b}`);
   }
 
 }
